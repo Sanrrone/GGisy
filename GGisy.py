4210608bf0ed61258329bb1313e54c2fd97bd487
Sanrrone <sandrolvalenzuelad@gmail.com>
2017-07-28 15:40:32 -0400

    version 2

--- GGisy.py
+++ GGisy.py
@@ -30,7 +30,7 @@ def main():
 	parser.add_option("-t","--threads",dest="Threads",help="Number of threads to be used for blast [default: 4]",default=4)
 	parser.add_option("-b","--blastout",dest="Blastout",help="Blast output file to be used instead doing it [default: none]",default=None)
 	parser.add_option("-c","--clean",dest="clean",help="clean files after execution [default: True]",default=True)
-
+	parser.add_option("-p","--contigLabel",dest="clabel",help="print contig label [default: True]",default=True)
 
 	(options,args) = parser.parse_args()
 
@@ -42,6 +42,7 @@ def main():
 	threads= str(options.Threads) #for subcallproccess must be str()
 	blastout= options.Blastout #dont cast to str
 	cleanf=options.clean
+	clabel=options.clabel
 
 	#check variables
 	if not genome1 or genome1 is None:
@@ -80,8 +81,8 @@ def main():
 		print "No Rscript was found, make sure is in your $PATH"
 		sys.exit()
 
-	Inputs = collections.namedtuple('Inputs', ['v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8'])
-	I = Inputs(genome1, genome2, alignL, evalue, Identity, threads, blastout, cleanf)
+	Inputs = collections.namedtuple('Inputs', ['v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8', 'v9'])
+	I = Inputs(genome1, genome2, alignL, evalue, Identity, threads, blastout, cleanf, clabel)
 	return I
 
 def which(program): #function to check if some program exists 
@@ -113,7 +114,7 @@ def blasting(genome1, genome2, evalue, threads):
 
 def filterBlastOutput(blastout,alignL,evalue,identity):
 
-	PARSED=open("parsed.tsv",'w') #overwrite if exist
+	PARSED=open("synteny.tsv",'w') #overwrite if exist
 	with open(blastout) as tsvfile:
 		tsvreader = csv.reader(tsvfile, delimiter="\t")
 		for line in tsvreader:
@@ -142,16 +143,19 @@ def parsingGenomes(genome):
 	PARSED.close
 	return str(gname+"_info.tsv")
 
-def handleR(conn, reference, query):
+def handleR(conn, reference, query, alignL, clabel):
 
 	plotstep=open("handle.R", 'w')
 	plotstep.write("""rm(list=ls());
 library(OmicCircos)
 library(RColorBrewer)
+library(varhandle)
 args<-commandArgs()
 handlefile<-as.character(args[6])
 refname<-as.character(args[7])
 queryname<-as.character(args[8])
+filterl<-as.numeric(args[9])
+contiglabel<-as.character(args[10])
 handle<-read.table(handlefile,sep = "\\t",stringsAsFactors = F,check.names = F)
 ref<-read.table(refname,sep = "\\t",stringsAsFactors = F,check.names = F)
 query<-read.table(queryname,sep = "\\t", stringsAsFactors = F,check.names = F)
@@ -160,16 +164,54 @@ rownames(query)<-query$V1
 qryUniq<-unique(sort(handle$V1))
 refUniq<-unique(sort(handle$V2))
 ref<-ref[refUniq,]
+ref<-ref[with(ref, order(-V3, V1)), ]
 query<-query[qryUniq,]
+query<-query[with(query, order(+V3, V1)), ]
 data<-rbind(ref,query)
+refname<-unlist(strsplit(refname,"_info\\\.tsv"))[1]
+queryname<-unlist(strsplit(queryname,"_info\\\.tsv"))[1]
 lowId<-min(handle$V3)
 fhand<-handle[handle$V6<handle$V7,]
 rhand<-handle[handle$V6>handle$V7,]
 linkf<-data.frame(seg1=fhand$V1, start1=fhand$V4, end1=fhand$V5, seg2=fhand$V2, start2=fhand$V6, end2=fhand$V7)
 linkr<-data.frame(seg1=rhand$V1, start1=rhand$V4, end1=rhand$V5, seg2=rhand$V2, start2=rhand$V6, end2=rhand$V7)
+#fix reverse positions
+for(i in 1:nrow(linkr)){
+  contign<-unfactor(linkr[i,4])
+  contigl<-ref[contign,3]
+  linkr[i,5]<- contigl-linkr[i,5]+1
+  linkr[i,6]<- contigl-linkr[i,6]+1
+}
 data["V5"]<-data["V4"]<-1
 colnames(data)<- c("chr", "start", "end","V4","V5")
-tocir <- segAnglePo(data, seg=c(as.matrix(data["chr"][,])))
+tocir <- segAnglePo(data, seg=data$chr)
+faketocir <- tocir
+faketocir[,1]<-""
+gl<-sum(data$end)
+#define spacer
+spacer<-as.integer(358/nrow(data))
+for(i in 1:(nrow(tocir)-1)){
+  #358 is the total angles (aviable) for all
+  tocir[i,"angle.end"]<-as.character(as.numeric(tocir[i,"angle.start"]) + (358/gl)*as.numeric(tocir[i,7]))
+  tocir[i+1,"angle.start"]<-as.character(as.numeric(tocir[i,"angle.end"])+ spacer)
+}
+tocir[i+1,"angle.end"]<-as.character(as.numeric(tocir[i+1,"angle.start"]) + (358/gl)*as.numeric(tocir[i,7]))
+refang<-as.numeric(tocir[1:nrow(ref),2])
+qryang<-as.numeric(tocir[(nrow(ref)+1):(nrow(ref)+nrow(query)),2])
+maxangr<-max(refang)
+for(i in 1:nrow(tocir)){
+  if(270+(maxangr-270)/2<as.numeric(tocir[i,2])){
+    break
+  }
+}
+faketocir[i,1]<-refname
+maxangq<-max(qryang)
+for(i in 1:nrow(tocir)){
+  if(maxangr+(maxangq-maxangr)/2<as.numeric(tocir[i,2])){
+    break
+  }
+}
+faketocir[i,1]<-queryname
 colors<-rev(colorRampPalette(rev(brewer.pal(n = 7, name = "RdYlBu")))(20))
 delta<-(100-lowId)/20
 scaleColors<- function(x){
@@ -189,48 +231,101 @@ addalpha <- function(col, alpha=1){
     stop("Please provide a vector of colours.")
   apply(sapply(col, col2rgb)/255, 2, 
         function(x) 
-          rgb(x[1], x[2], x[3], alpha=alpha))  
+          rgb(x[1], x[2], x[3], alpha=alpha))
 }
+black<-addalpha("#000000",0.8)
 colors<-addalpha(colors,0.8)
 linkf[,"colors"]<-addalpha(scaleColors(fhand$V3),0.8)
 linkr[,"colors"]<-addalpha(scaleColors(rhand$V3),0.8)
 pdf(file="synteny.pdf", width = 10, height =10)
-par(mar=c(2,2,2,2))
-xorigin=600
-yorigin=740
-plot(c(0,1500), c(0,1500), type="n", axes=FALSE, xlab="", ylab="", main="")
-circos(R=450, cir=tocir, W=10,type="chr", print.chr.lab=T, scale=F,xc = xorigin,yc = yorigin,
-       col = c(rep("dark blue",nrow(ref)),rep("#FEE496",nrow(query))),cex = 10)
-circos(R=445, cir=tocir, W=10, mapping=linkf , type="link.pg", lwd=1, col=linkf$colors,xc = xorigin,yc = yorigin)
-circos(R=445, cir=tocir, W=10, mapping=linkr , type="link.pg", lwd=1, col=linkr$colors,xc = xorigin,yc = yorigin)
-newlinkr<-linkr
-newlinkr$start1<-(newlinkr$end1-newlinkr$start1)/2
-newlinkr$start2<-(newlinkr$start2-newlinkr$end2)/2
-black<-addalpha("#000000",0.8)
-#circos(R=445, cir=tocir, W=10, mapping=newlinkr , type="link2", lwd=1, col=black,xc = xorigin,yc = yorigin)
 
-legend(x = 1230, y=1300, legend = c(unlist(strsplit(refname,"_info\\\.tsv"))[1],unlist(strsplit(queryname,"_info\\\.tsv"))[1]),
-       ncol = 1, cex = 0.8,  bty="n",
-       fill=c("dark blue","#FEE496"),
-       border = c("dark blue","#FEE496"),text.width=c(0.5,0.5),
-       title=expression(bold("Sequences")))
-legend(x = 1200, y=1100, legend = c("100","","","","","","","","","",(100-lowId)/2 + lowId,"","","","","","","","",lowId),
-       ncol = 1, cex = 0.8,  bty="n",
-       fill=colors,
-       border = colors,
-       y.intersp = 0.5,
-       x.intersp = 0.5,text.width=c(0.5,0.5),
-       title=expression(bold("Identity percent\\n")))
+if(contiglabel=="True"){
+  par(mar=c(2,2,2,2))
+  xorigin=700
+  yorigin=1000
+  plot(c(0,2000), c(0,2000), type="n", axes=FALSE, xlab="", ylab="", main="")
+  
+  circos(R=450, cir=tocir, W=10,type="chr", print.chr.lab=T, scale=F,xc = xorigin,yc = yorigin,
+           col = c(rep("dark blue",nrow(ref)),rep("#FEE496",nrow(query))),cex = 5)
 
+  circos(R=440, cir=tocir, mapping=linkf , type="link.pg", lwd=0.5, col=linkf$colors,xc = xorigin,yc = yorigin)
+  circos(R=440, cir=tocir, mapping=linkr , type="link.pg", lwd=0.5, col=linkr$colors,xc = xorigin,yc = yorigin)
+  newlinkr<-linkr
+  newlinkr$start1<-newlinkr$start1+as.integer((newlinkr$end1-newlinkr$start1)/2)+1
+  newlinkr$start2<-newlinkr$start2+as.integer((newlinkr$end2-newlinkr$start2)/2)-1
+  circos(R=440, cir=tocir, W=10, mapping=newlinkr , type="link", lwd=0.5, col=black,xc = xorigin,yc = yorigin)
+  
+  legend(x = 1500, y=1700, legend = c(refname,queryname),
+         ncol = 1, cex = 0.8,  bty="n",
+         fill=c("dark blue","#FEE496"),
+         border = c("dark blue","#FEE496"),text.width=c(0.5,0.5),
+         title="Sequences")
+  legend(x = 1430, y=1500, legend = c(paste("Reference: ", nrow(ref), " (", sum(ref$V3), " bp)", sep = ""), paste("Query: ",nrow(query), " (", sum(query$V3), " bp)", sep="")),
+         ncol = 1, cex = 0.8,  bty="n",
+         fill=c("dark blue","#FEE496"),
+         border = c("dark blue","#FEE496"),text.width=c(0.5,0.5),
+         title=paste("Contigs align >= ", filterl, " bp", sep=""))
+  legend(x = 1520, y=1300, legend = c("Forward","Reverse"),lty = c(0,1),merge=T,seg.len = 0.6,
+         ncol = 1, cex = 0.8,  bty="n",
+         fill="white",
+         border = "black",text.width=c(0.5,0.5),
+         title="Strand Match\\n(on reference)")
+  legend(x = 1505, y=1100, legend = c("100","","","","","","","","","",(100-lowId)/2 + lowId,"","","","","","","","",lowId),
+         ncol = 1, cex = 0.8,  bty="n",
+         fill=colors,
+         border = colors,
+         y.intersp = 0.5,
+         x.intersp = 0.5,text.width=c(0.5,0.5),
+         title="Identity percent\\n")
+}else{
+  
+  par(mar=c(2,2,2,2))
+  xorigin=750
+  yorigin=550
+  plot(c(0,1500), c(0,1500), type="n", axes=FALSE, xlab="", ylab="", main="")
+
+  circos(R=450, cir=faketocir, W=10,type="chr", print.chr.lab=T, scale=F,xc = xorigin,yc = yorigin,
+         col = "white")
+  circos(R=410, cir=tocir, W=10,type="chr", print.chr.lab=F, scale=F,xc = xorigin,yc = yorigin,
+         col = c(rep("dark blue",nrow(ref)),rep("#FEE496",nrow(query))),cex = 5)
+  
+  highlightr <- c(420, 450, tocir[1,1], 1, tocir[nrow(ref),1], tocir[nrow(ref),7], "dark blue", NA)
+  highlightq <- c(420, 450, query[1,1], 1, query[nrow(query),1], query[nrow(query),3], "#FEE496", NA)
+  circos(cir=tocir, mapping=highlightr, type="hl",xc = xorigin,yc = yorigin)
+  circos(cir=tocir, mapping=highlightq, type="hl",xc = xorigin,yc = yorigin)
+  
+  circos(R=400, cir=tocir, mapping=linkf , type="link.pg", lwd=0.5, col=linkf$colors,xc = xorigin,yc = yorigin)
+  circos(R=400, cir=tocir, mapping=linkr , type="link.pg", lwd=0.5, col=linkr$colors,xc = xorigin,yc = yorigin)
+  newlinkr<-linkr
+  newlinkr$start1<-newlinkr$start1+as.integer((newlinkr$end1-newlinkr$start1)/2)+1
+  newlinkr$start2<-newlinkr$start2+as.integer((newlinkr$end2-newlinkr$start2)/2)-1
+  circos(R=400, cir=tocir, W=10, mapping=newlinkr , type="link", lwd=0.3, col=black,xc = xorigin,yc = yorigin)
+  
+  legend(x = 10, y=1500, legend = c(paste("Reference: ", nrow(ref), " (", sum(ref$V3), " bp)", sep = ""), paste("Query: ",nrow(query), " (", sum(query$V3), " bp)", sep="")),
+         ncol = 1, cex = 0.8,  bty="n",
+         fill=c("dark blue","#FEE496"),
+         border = c("dark blue","#FEE496"),text.width=c(0.5,0.5),
+         title=paste("Contigs align >= ", filterl, " bp", sep=""))
+  legend(x = 135, y=1300, legend = c("Forward","Reverse"),lty = c(0,1),merge=T,seg.len = 0.6,
+         ncol = 1, cex = 0.8,  bty="n",
+         fill="white",
+         border = "black",text.width=c(0.5,0.5),
+         title="Strand Match\\n(on reference)")
+  legend(x = 960, y=1500, legend = c("100","","","","","","","","","",(100-lowId)/2 + lowId,"","","","","","","","",lowId),
+         ncol = 1, cex = 0.8,  bty="n",
+         fill=colors,
+         border = colors,
+         y.intersp = 0.5,
+         x.intersp = 0.5,text.width=c(0.5,0.5),
+         title="Identity percent\\n")
+}
 dev.off()""")
 	plotstep.close()
-	subprocess.call(["Rscript", "handle.R", conn, reference, query, "--vanilla"])
+	subprocess.call(["Rscript", "handle.R", conn, reference, query, str(alignL), str(clabel), "--vanilla"])
 
 
 def cleanfiles(ginfo1, ginfo2):
 
-	if os.path.isfile("parsed.tsv"):
-		os.remove("parsed.tsv")
 	if os.path.isfile("tmp.tsv"):
 		os.remove("tmp.tsv")
 	if os.path.isfile("ref.nin"):
@@ -255,7 +350,7 @@ if __name__ == '__main__':
 	filterBlastOutput(blastout=blastout, alignL=mainV.v3, evalue=mainV.v4, identity=mainV.v5)
 	ref=parsingGenomes(genome=mainV.v1)
 	que=parsingGenomes(genome=mainV.v2)
-	handleR(conn="parsed.tsv",reference=ref, query=que)
+	handleR(conn="synteny.tsv",reference=ref, query=que, alignL=mainV.v3, clabel=mainV.v9)
 	
 	if mainV.v8 == True:
 		cleanfiles(ref,que)
